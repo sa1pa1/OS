7975a35e8d01c9bdca80e12c38795e9d8d356016
Sally Bui <sal@Sallys-Air-2.modem>
2024-08-06 22:01:51 +0930

    updated shell

--- minishell.c
+++ minishell.c
@@ -18,7 +18,7 @@
 
 #define NV 20			/* max number of command tokens */
 #define NL 100			/* input buffer size */
-char            line[NL];	/* command input buffer */
+char line[NL];	/* command input buffer */
 
 
 /*
@@ -44,7 +44,8 @@ main(int argk, char *argv[], char *envp[])
   char           *v[NV];	/* array of pointers to command line tokens */
   char           *sep = " \t\n";/* command line token separators    */
   int             i;		/* parse index */
-
+  int             cwd[1024];
+  int num; //no of tokens in command line
 
   /* prompt for and process one command line at a time  */
 
@@ -63,31 +64,58 @@ main(int argk, char *argv[], char *envp[])
       continue;			/* to prompt */
 
     v[0] = strtok(line, sep);
-    for (i = 1; i < NV; i++) {
-      v[i] = strtok(NULL, sep);
-      if (v[i] == NULL)
-	break;
+    num = 1;
+    while (num < NV && v[num-1] != NULL){
+        v[num] = strtok(NULL,sep);
+        num++;
     }
+ if (strcmp(v[0], "cd") == 0) {
+            /* change the current working directory */
+            if (num > 1) {
+                if (chdir(v[1]) != 0) {
+                    perror("chdir");
+                }
+            } else {
+                /* go to the home directory */
+                if (chdir(getenv("HOME")) != 0) {
+                    perror("chdir");
+                }
+            }
+            continue;
+        }
     /* assert i is number of tokens + 1 */
 
     /* fork a child process to exec the command in v[0] */
 
     switch (frkRtnVal = fork()) {
     case -1:			/* fork returns error to parent process */
-      {
-	break;
-      }
+        {
+        perror("fork");
+        break;
+        }
     case 0:			/* code executed only by child process */
-      {
-	execvp(v[0], v);
-	
-      }
+        {
+        //check for & in last command 
+        if (strcmp(v[num-2],"&")==0){
+            v[num-2] = NULL;
+            num--;
+
+        }
+        execvp(v[0], v);
+        perror("execvp fail");
+        exit(1);
+        }
     default:			/* code executed only by parent process */
-      {
-	wpid = wait(0);
-	printf("%s done \n", v[0]);
-	break;
-      }
+        {
+        if (strcmp(v[num-2],"&")!=NULL){
+            wpid = wait(NULL);
+            printf("%s done \n", v[0]);
+        }else {
+            printf("[%d] %s\n",frkRtnVal, v[0]);
+        }
+        break;
+        }
     }				/* switch */
   }				/* while */
+  return 0;
 }				/* main */
