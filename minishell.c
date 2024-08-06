8eb673b772cff4961b0fbc215959692a12c54c55
Sally Bui <sal@Sallys-Air-2.modem>
2024-08-06 23:11:53 +0930

    updated shell

--- minishell.c
+++ minishell.c
@@ -1,3 +1,13 @@
+/*********************************************************************
+   Program  : miniShell                   Version    : 1.3
+ --------------------------------------------------------------------
+   skeleton code for linix/unix/minix command line interpreter
+ --------------------------------------------------------------------
+   File			: minishell.c
+   Compiler/System	: gcc/linux
+
+********************************************************************/
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -6,102 +16,112 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#define NV 20            /* max number of command tokens */
-#define NL 100            /* input buffer size */
-char line[NL];    /* command input buffer */
+#define NV 20			/* max number of command tokens */
+#define NL 100			/* input buffer size */
+char            line[NL];	/* command input buffer */
 
 
 /*
-    shell prompt
-*/
+	shell prompt
+ */
 
 void prompt(void)
 {
-    fprintf(stdout, "\n msh> ");
-    fflush(stdout);
+  fflush(stdout);
 }
 
-int main(int argc, char *argv[], char *envp[])
-/* argc - number of arguments */
+void background_handler(int signal)
+{
+    int status;
+    pid_t pid;
+
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
+        fprintf(stdout, "Background process %d finished\n", pid);
+    }
+}
+
+
+int main(int argk, char *argv[], char *envp[])
+/* argk - number of arguments */
 /* argv - argument vector from command line */
 /* envp - environment pointer */
 
 {
-    int frkRtnVal;    /* value returned by fork sys call */
-    int wpid;        /* value returned by wait */
-    char *v[NV];    /* array of pointers to command line tokens */
-    char *sep = " \t\n";/* command line token separators */
-    int i;        /* parse index */
-    char cwd[1024]; /* current working directory */
-    int numTokens; /* number of tokens in command line */
+  int             frkRtnVal;	/* value returned by fork sys call */
+  int             wpid;		/* value returned by wait */
+  char           *v[NV];	/* array of pointers to command line tokens */
+  char           *sep = " \t\n";/* command line token separators    */
+  int             i;		/* parse index */
+  int             bp;       /*background process*/
 
-    /* prompt for and process one command line at a time */
+  signal(SIGCHLD, background_handler);
 
-    while (1) {            /* do Forever */
-        prompt();
-        fgets(line, NL, stdin);
-        fflush(stdin);
+  /* prompt for and process one command line at a time  */
 
-        if (feof(stdin)) {        /* non-zero on EOF */
-            fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
-             feof(stdin), ferror(stdin));
-            exit(0);
-        }
-        if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-            continue;            /* to prompt */
-
-        /* tokenize the command line */
-        v[0] = strtok(line, sep);
-        numTokens = 1;
-        while (numTokens < NV && v[numTokens-1] != NULL) {
-            v[numTokens] = strtok(NULL, sep);
-            numTokens++;
-        }
-
-        /* handle built-in commands */
-        if (strcmp(v[0], "cd") == 0) {
-            /* change the current working directory */
-            if (numTokens > 1) {
-                if (chdir(v[1]) != 0) {
-                    perror("chdir");
+  while (1) {			/* do Forever */
+    prompt();
+    fgets(line, NL, stdin);
+    fflush(stdin);
+    if (fgets(line, NL, stdin) == NULL) {
+                if (feof(stdin)) {  /* non-zero on EOF  */
+                    exit(0);
+                } else {
+                    perror("fgets");
+                    continue;
                 }
-            } else {
-                /* go to the home directory */
-                if (chdir(getenv("HOME")) != 0) {
-                    perror("chdir");
-                }
+            }
+    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
+      continue;			/* to prompt */
+
+    v[0] = strtok(line, sep);
+    for (i = 1; i < NV; i++) {
+      v[i] = strtok(NULL, sep);
+      if (v[i] == NULL)
+	break;
+    }
+    /* assert i is number of tokens + 1 */
+
+    if (strcmp(v[0], "cd") == 0) {
+            if (v[1] == NULL || chdir(v[1]) != 0) {
+                perror("cd");
             }
             continue;
         }
 
-        /* fork a child process to exec the command in v[0] */
-        switch (frkRtnVal = fork()) {
-            case -1:            /* fork returns error to parent process */
-                perror("fork");
-                break;
-            case 0:            /* code executed only by child process */
-                /* check if the last token is "&" */
-                if (strcmp(v[numTokens-2], "&") == 0) {
-                    /* put the command in background */
-                    v[numTokens-2] = NULL;
-                    numTokens--;
+        bp = (i > 1 && strcmp(v[i-1], "&") == 0);
+        if (bp) {
+            v[i-1] = NULL;
+        }
+
+    /* fork a child process to exec the command in v[0] */
+
+    switch (frkRtnVal = fork()) {
+    case -1:			/* fork returns error to parent process */
+      {
+    perror("fork");
+	break;
+      }
+    case 0:			/* code executed only by child process */
+      {
+	if (execvp(v[0], v) == -1) {
+                perror("execvp fail");
+                exit(EXIT_FAILURE);
+            }
+	
+      }
+    default:			/* code executed only by parent process */
+      {
+    if (bp) {
+                printf("Background process %d started\n", frkRtnVal);
+            } else {
+                wpid = wait(0);
+                if (wpid == -1) {
+                    perror("wait");
                 }
-                /* execute the command */
-                execvp(v[0], v);
-                /* if execvp fails, terminate the child process */
-                perror("execvp");
-                exit(1);
-            default:            /* code executed only by parent process */
-                if (strcmp(v[numTokens-2], "&") != 0) {
-                    /* wait for the child process to finish */
-                    wpid = wait(NULL);
-                    printf("%s done \n", v[0]);
-                } else {
-                    /* report that the command is in background */
-                    printf("[%d] %s\n", frkRtnVal, v[0]);
-                }
-                break;
-        }                /* switch */
-    }                /* while */
-    return 0;
-}                /* main */
+                printf("%s done\n", v[0]);
+            }
+	break;
+      }
+    }				/* switch */
+  }				/* while */
+}				/* main */
