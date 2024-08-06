375419bc31a391ba421ac0f8d2ae97a4a1a276cc
Sally Bui <sal@Sallys-MacBook-Air-2.local>
2024-08-06 16:52:39 +0930

    files

--- even.c
+++ even.c
@@ -3,37 +3,29 @@
 #include <unistd.h>
 #include <signal.h>
 
-  void handle_sigint(int signal){
-        if(signal == SIGHUP){
+void handle_sighup(int signal){
             printf("Ouch!\n");
-       
-        }else if(signal == SIGINT){
-            printf("Yeah!\n");
-      
-        }
     }
-int main(){
-    int n;
-    scanf("%d",&n);
-    int nn = n;
-   if (nn<=0){
-    fprintf(stderr, "Enter a positive integer\n");
-    return 1;
-   }
 
- struct sigaction sa;
-    sa.sa_handler = handle_sigint;
-    sa.sa_flags = 0; 
-    sigemptyset(&sa.sa_mask);
-    
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGHUP, &sa, NULL);
+void handle_sigint(int signal){
+            printf("Yeah!\n");
+    }
+
+int main(int even, char *arg[]){
+    int n = atoi(arg[1]);
+    if (n<=0){
+        fprintf(stderr, "Enter a positive integer\n");
+        return 1;
+        }   
+
+    signal(SIGINT, handle_sigint);
+    signal(SIGHUP, handle_sighup);
 
 
-   for ( int i =0; i < nn;i++){
-    printf("%d\n",i*2);
-    sleep(5);
-   }
+    for ( int i =0; i < n;i++){
+        printf("%d\n",i*2);
+        sleep(5);
+    }
 
 return 0;
 
